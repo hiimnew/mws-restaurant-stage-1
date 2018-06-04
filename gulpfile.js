7d6406b4cf69a6ce1feaab7403b0b04054efb2e9
Ertan Kara <kara.ertan9@gmail.com>
2018-06-04 16:06:50 +0300

    Add responsive images

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+const imageResize = require('gulp-image-resize');
+
+
+gulp.task('resize', function() {
+  gulp.src('img/src/*')
+    .pipe(imageResize({
+      width: 500,
+      height: 600,
+      crop: true,
+      upscale: false
+
+    }).on('error', function(e) {
+      console.log(e);
+    }))
+  .pipe(gulp.dest('img/resized_img'));
+});
+
+gulp.task('compress',function() {
+  gulp.src('img/resized_img/*')
+    .pipe(imagemin([
+      imagemin.jpegtran({progressive: true})
+    ]))
+    .pipe(gulp.dest('img/compressed'));
+});
+
+gulp.task('compress_large', function() {
+  gulp.src('img/src/*')
+  .pipe(imagemin([
+    imagemin.jpegtran({progressive: true})
+  ]))
+    .pipe(gulp.dest('img/compressed_large'))
+});
+
